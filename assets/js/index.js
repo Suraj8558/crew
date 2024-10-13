b6f726e923530bfad1d813d1d49e0473fd023338
suraj <surajsk8558@gmail.com>
2024-10-13 15:43:47 +0530

    Completed UI of pricing  faq and footer section

---
+++ assets/js/index.js
@@ -0,0 +1,14 @@
+const accordionItems = document.querySelectorAll(".accordion-item");
+
+accordionItems.forEach((item) => {
+	const header = item.querySelector(".accordion-header");
+	header.addEventListener("click", () => {
+		const openItem = document.querySelector(".accordion-item.active");
+
+		if (openItem && openItem !== item) {
+			openItem.classList.remove("active");
+		}
+
+		item.classList.toggle("active");
+	});
+});
